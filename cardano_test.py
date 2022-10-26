a6429bd29376fb4287e6e72a0f798c4b4fa5fdd0
untitled888 <larion.novikov@mail.ru>
2022-10-26 20:45:33 +0300

    Добавил ключ в тест

--- cardano_test.py
+++ cardano_test.py
@@ -1,14 +1,25 @@
 import cardano
 import unittest
 
+key = [
+    [0, 1, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1]
+]
+
 text = ["осволрбп", "риеарцжй", "аонидния", "аекеонет", "адмамеен", "лттнеооп", "нигратае", "пебтдвуо"]
 cardano.size = 8
 
 class Test(unittest.TestCase):
     def test_decrypt(self):
-        self.assertEqual(cardano.decrypt(text, cardano.key, "CW"), "собраниеделегатоврайонаотменитеполициякемтопредупрежденаантонабв")
+        self.assertEqual(cardano.decrypt(text, key, "CW"), "собраниеделегатоврайонаотменитеполициякемтопредупрежденаантонабв")
     def test_ccw(self):
-        self.assertEqual(cardano.decrypt(text, cardano.key, "CCW"), "собраниеделегатопрежденаантонабволициякемтопредуврайонаотменитеп")
+        self.assertEqual(cardano.decrypt(text, key, "CCW"), "собраниеделегатопрежденаантонабволициякемтопредуврайонаотменитеп")
 
 if __name__ == '__main__':
     unittest.main()
