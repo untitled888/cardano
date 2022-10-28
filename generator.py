0a516f08c2f1f79bdd419ea4828144a9485751ab
untitled888 <larion.novikov@mail.ru>
2022-10-28 22:37:26 +0300

    Написал генератор слов

--- generator.py
+++ generator.py
@@ -1,1 +1,27 @@
-import tkinter
+text = ''
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя \n'
+processedText = ''
+
+while True:
+    part = input('Введите часть текста:\n')
+    if part.lower() == "стоп":
+        break
+    text += part.lower()
+    text += ' '
+
+for i in text:
+    if i in alphabet:
+        processedText += i
+
+processedText = processedText.replace('\n', ' ')
+splitText = processedText.split()
+del processedText
+
+words = []
+
+for i in splitText:
+    if len(i) > 4:
+        words.append(i)
+
+words = set(words)
+print(words)
